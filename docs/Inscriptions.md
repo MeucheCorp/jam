86763a22628e0a4924b2e9c0efa71bc990c3963a
Jules Fouchy <45451201+JulesFouchy@users.noreply.github.com>
2024-04-22 15:15:47 +0200

    🎉 Initial commit

---
+++ docs/Inscriptions.md
@@ -0,0 +1,6 @@
+:::info
+Les inscriptions ouvriront bientôt, pour ne pas les louper :
+- Venez sur le Discord
+- Rejoignez la jam sur itch.io
+- Inscrivez-vous sur la mailing list pour ne rater aucune info :
+:::
---
+++ docusaurus.config.ts
@@ -0,0 +1,98 @@
+import { themes as prismThemes } from "prism-react-renderer"
+import type { Config } from "@docusaurus/types"
+import type * as Preset from "@docusaurus/preset-classic"
+
+const config: Config = {
+  title: "MeucheJam",
+  tagline: "",
+  favicon: "img/favicon.ico",
+
+  url: "https://jam.meucheroume.fr/",
+  baseUrl: "/",
+
+  organizationName: "MeucheCorp",
+  projectName: "jam",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  i18n: {
+    defaultLocale: "fr",
+    locales: ["fr"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          routeBasePath: "/",
+        },
+        blog: false,
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: "img/logo_meuche_sans_fond - Copy.png",
+    navbar: {
+      title: "MeucheJam",
+      logo: {
+        alt: "MeucheRoume",
+        src: "img/logo_meuche_sans_fond - Copy.png",
+      },
+      items: [{ to: "/Inscriptions", label: "Inscriptions", position: "left" }],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+}
+
+export default config
